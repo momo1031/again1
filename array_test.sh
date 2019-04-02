5b2896b584bba04e35302a2b34d7a8e1054544c8
momo1031 <48973405+momo1031@users.noreply.github.com>
2019-04-02 15:49:45 +0900

    Update array_test.sh

--- array_test.sh
+++ array_test.sh
@@ -1,1 +1,18 @@
-i
+#!/bin/bash
+declare -a words
+i=0
+while [ 1 ]
+do
+  read -p "Enter a new word: " go
+  if [ $go == "quit" ]
+  then
+    exit 1
+  elif [ $go == "list" ]
+  then
+    echo ${words[*]}
+  else
+    words[i] = go
+    let i=i+1
+    echo "Length:" $i
+   fi
+then
